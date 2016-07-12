d586ac0811d1769beec9064c1aa8baa8723cae60
Matthias Urlichs <matthias@urlichs.de>
2016-07-12 08:21:10 +0200

    * Add a .when_disconnected future to the AMQP protocol
    so that HL code can do something about it without overriding the protocol
    class

--- qbroker/__init__.py
+++ qbroker/__init__.py
@@ -13,7 +13,7 @@ from __future__ import absolute_import, print_function, division, unicode_litera
 ## Thus, please do not remove the next line, or insert any blank lines.
 ##BP
 
-__VERSION__ = (0,7,1)
+__VERSION__ = (0,7,2)
 
 
 # Python 3.5 deprecates .async in favor of .ensure_future
