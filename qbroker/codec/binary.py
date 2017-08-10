ce4e41b33d855b200f43033f9b1e2faa7369a59e
Matthias Urlichs <matthias@urlichs.de>
2017-08-10 08:14:57 +0200

    binary codec: return received data as bytes, not Unicode

--- qbroker/codec/binary.py
+++ qbroker/codec/binary.py
@@ -6,8 +6,6 @@ from __future__ import print_function,absolute_import
 CODEC = "application/binary"
 
 def encode(data):
-	if isinstance(data,bytes):
-		data = data.decode('utf-8')
 	return data
 
 def decode(data):
